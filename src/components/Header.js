43cdf973a0e2b244e8d7f846dfe818505ebe7bd3
Ruslan Andreev <ruslan.andreev@avionero.com>
2020-01-25 17:31:02 +0300

    Update data

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ function Header() {
             <img 
               src="https://cdn.buymeacoffee.com/buttons/lato-red.png" 
               alt="Buy Me A Coffee" 
-              style={{height: '51px', width: '217px', borderRadius: '3px'}} 
+              style={{height: '40px', width: 'auto', borderRadius: '3px'}} 
             />
           </a>
           </HeaderContainer>
